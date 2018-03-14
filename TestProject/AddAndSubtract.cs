6b8893653c1fa53159179e56d3f6cb36ec683b1c
Gupta <120054@NTTDATA.COM>
2018-03-14 13:55:09 +0530

    adding files

---
+++ TestProject/AddAndSubtract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+   public class AddAndSubtract
+    {
+       public int add(int i, int j)
+       {
+           return i + j;
+           
+       }
+       public int Subtract(int i, int j)
+       {
+           return i - j;
+       }
+    }
+}
---
+++ TestProject/TestProject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestProject
+{
+    [TestClass]
+    public class TestProject
+    {
+        [TestMethod]
+        public void TestingADDFunctionality()
+        {
+            AddAndSubtract objAddAndSubtract = new AddAndSubtract();
+            int a = 10;
+            int b = 20;
+
+            int output = 0;
+            output = objAddAndSubtract.add(a, b);
+
+            Assert.AreEqual("30", output, "Add functionality tested");
+           
+
+        }
+        [TestMethod]
+        public void TestingSubFunctionality()
+        {
+            AddAndSubtract objAddAndSubtract = new AddAndSubtract();
+            int a = 20;
+            int b = 10;
+
+            int output = 0;
+            output = objAddAndSubtract.Subtract(a, b);
+            Assert.AreEqual("10", output, "Sub functionality tested");
+        }
+    }
+}
